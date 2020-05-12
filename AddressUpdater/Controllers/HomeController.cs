ac998d61d685c67c2acb55eee0d7bcfea6198c42
Imaya Kumar Jagannathan <40274599+awsimaya@users.noreply.github.com>
2020-05-12 17:21:53 -0400

    First push

---
+++ AddressUpdater/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AddressUpdater.Models;
+using System.Threading;
+
+namespace AddressUpdater.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstName, string lastName, string address)
+        {
+            var rnd = new Random().Next(0, 5);
+
+            if (rnd > 3)
+            {
+                ViewData.Add("status", "failure");
+            }
+            else
+            {
+                ViewData.Add("status", "success");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
